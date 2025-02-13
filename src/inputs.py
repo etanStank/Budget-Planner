8829d9acd479f169f3df3b0fa20e82de86eccb95
John <20042@jhc.school.nz>
2025-02-14 09:47:44 +1300

    Fixed and completed the convert_string.     Works on edge cases,     Fixed the extra bugs where the index wouldn't work (I was checking the wrong list....),     Addeds loads of comments,     Finished all of my multidimensional list, and it works,     First half of the multidimensional list is for the prefix, the second half of the multidimensional list is for the request
    on a side note, I think there is more comments than code..
    
    and i truly understand that this was a mistake

--- src/inputs.py
+++ src/inputs.py
@@ -28,33 +28,38 @@ __prefixes__ = ["o", "a", "t", "c"]
 def convert_string(user_input):
     # Converts the initial input into a multidimensional list which seperates the prefix from the request.
     # Doesn't check edge cases but eh, should check for all prefixes...
+    # IT WORKS!!
     List = [[],[]]
     input_list = list(user_input)
-    print(input_list)
 
-    print("HERRO 2")
+    # Loops through all of the items in the input_list
     for item in input_list:
+        # Gets item index
         item_index = input_list.index(item)
+        # Checks for - as it is apart of the prefix
         if item == "-":
-            print("HERRO 123")
-            print(f"Item Index: {item_index}")
+            # Gets the item after the -
             item_afterIndex = item_index + 1
-            item_after = List[item_afterIndex]
-            print(f"Item after: {item_after}")
+            item_after = input_list[item_afterIndex]
             if isinstance(item_after, str):
-                print("ASD")
+                # Checks that it is a str, and then check if it's apart of __prefixes__
                 if item_after in __prefixes__:
-                        print("HERRO 3")
+                        # Adds the prefix to the first part of the multidimensional list
                         List[0].append(str(item) + str(item_after))
-    return List
+                        # Removes prefixes from input_Lists
+                        input_list.remove(item)
+                        input_list.remove(item_after)
+                        # Adds the rest of the input to the second part of the multidimensional list
+                        for instance in input_list:
+                             List[1].append(instance)
+                        return List
 
 # | ------------------------------------------------------------- | #
 
 def input_request():
     user_input = input("Add your request: ")
     if user_input:
-        convert_string(user_input)
-        print("HERRO")
+        return convert_string(user_input)
     else:
         print("Please enter a valid request.")
         input_request()
