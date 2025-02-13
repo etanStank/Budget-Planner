e4c1d837a82ab5ca39c7c5b520fa7cf1ba99a8b0
John <20042@jhc.school.nz>
2025-02-13 13:25:43 +1300

    Purely fixing convert_string function. ```Bugs found and fixed:     Import error (Removed, haven't added back yet)     Input list was using .split method so each item was only done through spaces, rather than each character (Fixed, changed to list method)     Prefixes wouldn't load due to the removal of the import, added a temporary local __prefixes__ table (Fixed, added file local prefixes table)     Known bug: Item_index is getting the item index, but is unable to be get the item_after index. (How to fix, idk yet.)
    Finished input_request function.
        Added edge case handlers,

--- src/inputs.py
+++ src/inputs.py
@@ -21,32 +21,46 @@ specify all of the prefixes
 
 import multiprocessing
 
-import budget_planner
+__prefixes__ = ["o", "a", "t", "c"]
 
 # | ------------------------------------------------------------- | #
 
 def convert_string(user_input):
     # Converts the initial input into a multidimensional list which seperates the prefix from the request.
     # Doesn't check edge cases but eh, should check for all prefixes...
-    list = [[],[]]
-    input_list = user_input.split()
-    
+    List = [[],[]]
+    input_list = list(user_input)
+    print(input_list)
+
+    print("HERRO 2")
     for item in input_list:
         item_index = input_list.index(item)
         if item == "-":
-            item_after = list[item_index + 1]
+            print("HERRO 123")
+            print(f"Item Index: {item_index}")
+            item_afterIndex = item_index + 1
+            item_after = List[item_afterIndex]
+            print(f"Item after: {item_after}")
             if isinstance(item_after, str):
-                if item_after in budget_planner.__prefixes__:
-                        list[0].append(str(item) + str(item_after))
-    return list
+                print("ASD")
+                if item_after in __prefixes__:
+                        print("HERRO 3")
+                        List[0].append(str(item) + str(item_after))
+    return List
 
 # | ------------------------------------------------------------- | #
 
 def input_request():
     user_input = input("Add your request: ")
-    convert_string(user_input)
+    if user_input:
+        convert_string(user_input)
+        print("HERRO")
+    else:
+        print("Please enter a valid request.")
+        input_request()
     # send info over to calculations
     # tell the main py file to not request input until it is complete with calculations
 
 # | ------------------------------------------------------------- | #
 
+print(input_request())
