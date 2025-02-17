9ba840258f1ab842105fc5226f772e493f79253c
John <20042@jhc.school.nz>
2025-02-18 10:46:01 +1300

    Added a new function called 'findMoneyInterger'     It works on basic stuff, but it's not perfect     It checks for $ (currency symbol) and then will check all of the items after it and find all of the numbers.

--- src/calculations.py
+++ src/calculations.py
@@ -14,9 +14,24 @@ time = []
 
 # | ------------------------------------------------------------- | #
 
+def findMoneyInterger(moneyList):
+    # Finds the money from the list
+    money_value = 0
+    for item in moneyList:
+        if item == "$":
+            currency_symbol_index = moneyList.index(item)
+            for number in range(currency_symbol_index, len(moneyList)):
+                if moneyList[number].isdigit():
+                    money_value = moneyList[int(number)] + int(money_value)
+                    continue
+                else:
+                    continue
+   
+    return money_value
+# | ------------------------------------------------------------- | #
 def calculate(__list__):
     prefix = __list__[0]
     data = __list__[1]
 
-
-# | ------------------------------------------------------------- | #
+    
+print(findMoneyInterger(["$", "1", "0", "0", "0"]))
